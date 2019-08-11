cce9ed0db384880714fa63968dc9dc07f042c3a5
Joaquim Alves <jafn92@gmail.com>
2019-08-11 08:44:35 -0300

    Update api.js
    https

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://apmob.herokuapp.com:3000'
+    baseURL: 'https://apmob.herokuapp.com:3333'
 });
 
 export default api;
